467acd9ceee7801d6b11399273b1508a488d7ecf
nat-netl <nat_netl3@bk.ru>
2022-08-25 16:19:44 +0300

    added new files in repository

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navigation() {
+  return (
+    <nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white'>
+      <h3 className='font-bold'>GitHub search</h3>
+      
+      <div>
+        <Link to='/' className='mr-2'>Home</Link>
+        <Link to='/favorites'>Favorites</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation
